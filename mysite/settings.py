100835f98916660ee17d951a840152e35e62a881
steffyjk <steffy.jk2018@gmail.com>
2023-02-06 17:00:09 +0530

    polls django custom app added into Installed apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # DJANGO APP [ POLLS ] CONFIG
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
